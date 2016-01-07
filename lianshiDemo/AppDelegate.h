51fea23e73abb791a04f025dc661897333ee1a5d
jiangtaidi <jiangtd@jiangtddemacbook-air.local>
2016-01-07 23:10:18 +0800

    Initial Commit

---
+++ lianshiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lianshiDemo
+//
+//  Created by jiangtd on 16/1/7.
+//  Copyright © 2016年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
